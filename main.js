92cfce1b96cdc7087ac1c8da46352a877516137f
jennvance <vancejenniferm@gmail.com>
2019-01-02 22:22:44 -0500

    recursion

--- main.js
+++ main.js
@@ -2,3 +2,11 @@
 function reverseString(str) {
   return str.split('').reverse().join('');
 }
+
+//Factorialize:
+function factorialize(num){
+  if(num ===0){
+    return 1;
+  }
+  return num * factorialize(num-1)
+}
