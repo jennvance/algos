3f61d76c354b9211ad716488bb7201ea949d47fb
jennvance <vancejenniferm@gmail.com>
2019-01-03 11:11:28 -0500

    proc/dec

--- main.js
+++ main.js
@@ -10,3 +10,34 @@ function factorialize(num){
   }
   return num * factorialize(num-1)
 }
+
+//Find largest item in each subarray (in this case, array contains 4 subarrays):
+
+//Procedural version:
+function largestOfFour(arr) {
+  var results = [];
+  for (var i = 0; i < arr.length; i++) {
+    var largestNumber = arr[i][0];
+    for (var j = 1; j < arr[i].length; j++) {
+      if (arr[i][j] > largestNumber) {
+        largestNumber = arr[i][j];
+      }
+    }
+	results[i] = largestNumber;
+  }
+  return results;
+}
+
+//Declarative version:
+function largestOfFour(arr) {
+  return arr.map(function(group){
+    return group.reduce(function(prev, current) {
+      return (current > prev) ? current : prev;
+    });
+  });
+}
+
+//Advanced declarative version:
+function largestOfFour(arr) {
+  return arr.map(Function.apply.bind(Math.max, null));
+}
